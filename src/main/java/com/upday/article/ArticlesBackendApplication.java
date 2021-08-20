d904015ffc5d71206d6bfe0f3d33e70d0772e8b0
Yogeshwar Bagul <mr.yogeshwarbagul@gmail.com>
2021-08-20 14:32:05 +0530

    init commit

---
+++ src/main/java/com/upday/article/ArticlesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.upday.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upday/article/ArticlesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upday.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
